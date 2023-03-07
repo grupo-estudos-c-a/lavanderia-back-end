28efb77e7d41d24ebf4231f431ea94a567250835
Amanda Muniz <dev.amandacsm@gmail.com>
2023-03-06 22:02:17 -0300

    adding getters and setter and standard methods

--- src/main/java/br/com/bicko/lavanderiaonline/entities/Order.java
+++ src/main/java/br/com/bicko/lavanderiaonline/entities/Order.java
@@ -1,16 +1,23 @@
 package br.com.bicko.lavanderiaonline.entities;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+
+@Getter
+@Setter
 @Entity
+@Builder
 @Table(name = "pedido")
+@RequiredArgsConstructor
 public class Order {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_pedido", nullable = false)
     private Integer id;
 
@@ -29,4 +36,5 @@ public class Order {
 
     @Column(name = "situacao_pedido", nullable = false)
     private String status; //descriptions came from Enum
+    
 }
