75623e248f36384a022e0995d7dbfb08cd6ea8af
Amanda Muniz <dev.amandacsm@gmail.com>
2023-03-06 21:14:12 -0300

    first commit

---
+++ src/main/java/br/com/bicko/lavanderiaonline/LavanderiaonlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.bicko.lavanderiaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavanderiaonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavanderiaonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bicko/lavanderiaonline/LavanderiaonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bicko.lavanderiaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavanderiaonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
